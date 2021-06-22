e1bd859036c2bfb99a5351f592043f7fe2c2dda7
rugbylin <76195692+rugbylin@users.noreply.github.com>
2021-06-22 16:42:38 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.Shake, function () {
+    time = "" + hours + (":" + minutes)
+    basic.showString(time)
+})
+let hours = 0
+let minutes = 0
+let time = ""
+let ampm = false
+time = ""
+let adjust = 0
+minutes = 0
+hours = 0
